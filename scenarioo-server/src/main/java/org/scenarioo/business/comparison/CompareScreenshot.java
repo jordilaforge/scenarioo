4d2ec208b2e2591f5b9b46f8920b00275b646dbb
jordilaforge <8899ph@web.de>
2014-12-04 20:30:47 +0100

    Added More TestCases Did Code Refactoring Started implementig Comparison for different Sizes of Images

--- scenarioo-server/src/main/java/org/scenarioo/business/comparison/CompareScreenshot.java
+++ scenarioo-server/src/main/java/org/scenarioo/business/comparison/CompareScreenshot.java
@@ -5,124 +5,101 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.GrayFilter;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.FileInputStream;
 
 import javax.imageio.ImageIO;
 import javax.swing.GrayFilter;
 
 public class CompareScreenshot {
 
-	// Attributes
-	protected int comparex = 10;
-	protected int comparey = 10;
-	protected int factorD = 10;
-	protected int tolerance = 50;
+	int threshold = 10;
 
-	// returns difference in percentage
-	// 0% = same image 100% = totally different image
-	public int compare(String fileA, String fileB){
+	public double compare(String fileA, String fileB) {
 		checkPreconditions(fileA, fileB);
-		Image imageA=loadImage(fileA);
-		Image imageB=loadImage(fileB);
-		int diffvalue = compareImages(imageA,imageB);
-		int maxdiff = calculateMaxDiff(imageA,imageB);
-		double percentage = calculatePercentage(diffvalue, maxdiff);
-		return (int) Math.round(percentage);
+		Image imageA = loadImage(fileA);
+		Image imageB = loadImage(fileB);
+		int scale = checkScale(imageA, imageB);
+		double diffvalue = compareImages(imageA, imageB, scale);
+		return diffvalue;
 	}
-	
-	
-	private double calculatePercentage(int diffvalue, int maxdiff) {
-		float percent = (diffvalue * 100.0f) / maxdiff;
+
+	private double compareImages(Image imageA, Image imageB, int scale) {
+
+		BufferedImage img1 = imageToBufferedImage(imageA);
+		BufferedImage img2 = imageToBufferedImage(imageB);
+		switch (scale) {
+		case 0:
+			return compareBufferedImages(img1, img2);
+		case 1:
+			return img1HeigtIsBigger(img1, img2);
+		case 2:
+			return 0.0;
+		case 3:
+			return 0.0;
+		case 4:
+			return 0.0;
+		case 5:
+			return 0.0;
+		case 6:
+			return 0.0;
+		default:
+			return 0.0;
+		}
+	}
+
+	private double img1HeigtIsBigger(BufferedImage img2, BufferedImage img1) {
+		int area_img1 = img1.getHeight() * img1.getWidth();
+		int area_img2 = img2.getHeight() * img2.getWidth();
+		int area_diff = Math.min(img1.getHeight(), img2.getHeight()) * Math.min(img1.getWidth(), img2.getWidth());
+		double percentageAreaDiff = area_diff / Math.max(area_img1, area_img2);
+		BufferedImage subimage = img2.getSubimage(img1.getMinX(), img1.getMinY(), img1.getWidth(), img1.getHeight());
+		double difference = compareBufferedImages(subimage, img1);
+		return (percentageAreaDiff / 100) * (difference / 100);
+	}
+
+	private double compareBufferedImages(BufferedImage img1, BufferedImage img2) {
+		if ((img1.getHeight() == img2.getHeight()) && (img1.getWidth() == img2.getWidth())) {
+			int PixelDiff = 0;
+			int totalPixel = img1.getHeight()*img1.getWidth();
+			for (int x = img1.getMinX(); x < img1.getWidth(); ++x) {
+				for (int y = img1.getMinY(); y < img1.getHeight(); ++y) {
+					int diff=Math.abs(img1.getRGB(x, y)-img2.getRGB(x, y));
+					if (diff > threshold) {
+						++PixelDiff;
+					}
+				}
+			}
+			return calculatePercentage(PixelDiff, totalPixel);
+		} else {
+			throw new IllegalArgumentException("Images not the Same Size");
+		}
+
+	}
+
+	private double calculatePercentage(int quotient, int divisor) {
+		if (divisor == 0) {
+			throw new IllegalArgumentException("'divisor' is 0");
+		}
+		float percent = (quotient * 100.0f) / divisor;
 		return percent;
-	}
-
-
-	public int compareImages(Image imageA, Image imageB) {
-		// convert to gray images.
-		BufferedImage img1 = imageToBufferedImage(GrayFilter
-				.createDisabledImage(imageA));
-		BufferedImage img2 = imageToBufferedImage(GrayFilter
-				.createDisabledImage(imageB));
-		checkScale(img1, img2);
-		int totaldiff = 0;
-		// how big are each section
-		int blocksx = (int) (img1.getWidth() / comparex);
-		int blocksy = (int) (img1.getHeight() / comparey);
-		// loop through whole image and compare individual blocks of images
-		for (int y = 0; y < comparey; y++) {
-			for (int x = 0; x < comparex; x++) {
-				int b1 = getAverageBrightness(img1.getSubimage(x * blocksx, y
-						* blocksy, blocksx - 1, blocksy - 1));
-				int b2 = getAverageBrightness(img2.getSubimage(x * blocksx, y
-						* blocksy, blocksx - 1, blocksy - 1));
-				int diff = Math.abs(b1 - b2);
-				totaldiff += diff;
-			}
-		}
-		return totaldiff;
-	}
-
-	private void checkScale(BufferedImage img1, BufferedImage img2) {
-		if ((img1.getHeight() != img2.getHeight())
-				&& (img1.getWidth() != img2.getWidth())) {
-			throw new IllegalArgumentException("Image scale is not the same");
-		}
-
-	}
-
-
-	public int calculateMaxDiff(Image bimg1,Image bimg2) {
-		BufferedImage orig1 = imageToBufferedImage(bimg1);
-		BufferedImage orig2 = imageToBufferedImage(bimg2);
-		BufferedImage im = new BufferedImage(orig1.getWidth(),orig1.getHeight(),BufferedImage.TYPE_BYTE_BINARY);
-		 // We need its raster to set the pixels' values.
-		     WritableRaster raster = im.getRaster();
-		     // Put the pixels on the raster. Note that only values 0 and 1 are used for the pixels.
-		     // You could even use other values: in this type of image, even values are black and odd
-		     // values are white.
-		     for(int h=0;h<orig1.getHeight();h++)
-		       for(int w=0;w<orig1.getWidth();w++)
-		         raster.setSample(w,h,0,0);
-		BufferedImage im2 = new BufferedImage(orig1.getWidth(),orig1.getHeight(),BufferedImage.TYPE_BYTE_BINARY);
-			 // We need its raster to set the pixels' values.
-			     WritableRaster raster2 = im2.getRaster();
-			     // Put the pixels on the raster. Note that only values 0 and 1 are used for the pixels.
-			     // You could even use other values: in this type of image, even values are black and odd
-			     // values are white.
-			     for(int h=0;h<orig1.getHeight();h++)
-			       for(int w=0;w<orig1.getWidth();w++)
-			         raster2.setSample(w,h,0,1);
-		return compareImages(im,im2);
-	}
-
-	private void checkPreconditions(String fileA, String fileB) {
-		if (fileA == null) {
-			throw new IllegalArgumentException(
-					"First file argument must not be null.");
-		}
-		if (fileB == null) {
-			throw new IllegalArgumentException(
-					"Second file argument must not be null.");
-		}
-	}
-
-	// returns a value specifying some kind of average brightness in the image.
-	protected int getAverageBrightness(BufferedImage img) {
-		Raster r = img.getData();
-		int total = 0;
-		for (int y = 0; y < r.getHeight(); y++) {
-			for (int x = 0; x < r.getWidth(); x++) {
-				total += r.getSample(r.getMinX() + x, r.getMinY() + y, 0);
-			}
-		}
-		return (int) (total / ((r.getWidth() / factorD) * (r.getHeight() / factorD)));
 	}
 
 	// buffered images are just better.
 	protected static BufferedImage imageToBufferedImage(Image img) {
-		BufferedImage bi = new BufferedImage(img.getWidth(null),
-				img.getHeight(null), BufferedImage.TYPE_INT_RGB);
+		BufferedImage bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_RGB);
 		Graphics2D g2 = bi.createGraphics();
 		g2.drawImage(img, null, null);
 		return bi;
@@ -144,5 +121,43 @@ public class CompareScreenshot {
 			io.printStackTrace();
 		}
 		return bi;
+	}
+
+	private void checkPreconditions(String fileA, String fileB) {
+		if (fileA == null) {
+			throw new IllegalArgumentException("First file argument must not be null.");
+		}
+		if (fileB == null) {
+			throw new IllegalArgumentException("Second file argument must not be null.");
+		}
+	}
+
+	// 0=same size
+	// 1=img1 height bigger
+	// 2=img1 width bigger
+	// 3=img1 height and width are bigger
+	// 4=img2 height bigger
+	// 5=img2 width bigger
+	// 6=img2 height and width are bigger
+	private int checkScale(Image img1, Image img2) {
+		int scale = 0;
+		BufferedImage bimg1 = imageToBufferedImage(img1);
+		BufferedImage bimg2 = imageToBufferedImage(img2);
+		if ((bimg1.getHeight() == bimg2.getHeight()) & (bimg1.getWidth() == bimg2.getWidth())) {
+			scale = 0;
+		} else if ((bimg1.getHeight() > bimg2.getHeight()) & (bimg1.getWidth() == bimg2.getWidth())) {
+			scale = 1;
+		} else if ((bimg1.getHeight() == bimg2.getHeight()) & (bimg1.getWidth() > bimg2.getWidth())) {
+			scale = 2;
+		} else if ((bimg1.getHeight() > bimg2.getHeight()) & (bimg1.getWidth() > bimg2.getWidth())) {
+			scale = 3;
+		} else if ((bimg1.getHeight() < bimg2.getHeight()) & (bimg1.getWidth() == bimg2.getWidth())) {
+			scale = 4;
+		} else if ((bimg1.getHeight() == bimg2.getHeight()) & (bimg1.getWidth() < bimg2.getWidth())) {
+			scale = 5;
+		} else if ((bimg1.getHeight() < bimg2.getHeight()) & (bimg1.getWidth() < bimg2.getWidth())) {
+			scale = 6;
+		}
+		return scale;
 	}
 }
--- scenarioo-server/src/test/java/org/scenarioo/business/comparison/CompareScreenshotTest.java
+++ scenarioo-server/src/test/java/org/scenarioo/business/comparison/CompareScreenshotTest.java
@@ -15,43 +15,48 @@ public class CompareScreenshotTest {
 	private static final String FILE_TOTAL_BLACK = new String("src/test/resources/image_total_black.png");
 	private static final String FILE_TOTAL_WHITE = new String("src/test/resources/image_total_white.png");
 	private static final String FILE_50_300 = new String("src/test/resources/image_50_300.png");
+	private static final String FILE_75_2000 = new String("src/test/resources/image_75_2000.png");
+	private static final String FILE_75_300 = new String("src/test/resources/image_75_300.png");
+	private static final String FILE_50_300_400 = new String("src/test/resources/image_50_300_400.png");
+	private static final String FILE_50_400_300 = new String("src/test/resources/image_50_400_300.png");
 	private static final String FILE_TOTAL_WHITE_300 = new String("src/test/resources/image_total_white_300.png");
 	private static final String FILE_TOTAL_BLACK_300 = new String("src/test/resources/image_total_black_300.png");
-	
-	
+	private static final String FILE_TOTAL_WHITE_2000 = new String("src/test/resources/image_total_white_2000.png");
+
 	private String imageA;
 	private String imageB;
-	private int differenceInPercent;
-	
+	private double differenceInPercent;
+	private double accuracy = 1.00;
+
 	@Test
 	public void ifFirstParameterIsNull_expectException() {
 		givenFirstParameterIsNullAndSecondIsSet();
-		
+
 		try {
 			whenComparingScreenshots();
 			Assert.fail();
-		} catch(IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) {
 			Assert.assertEquals("First file argument must not be null.", e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void ifSecondParameterIsNull_expectException() {
 		givenSecondParameterIsNullAndFirstIsSet();
-		
+
 		try {
 			whenComparingScreenshots();
 			Assert.fail();
-		} catch(IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) {
 			Assert.assertEquals("Second file argument must not be null.", e.getMessage());
 		}
 	}
-	
+
 	private void givenFirstParameterIsNullAndSecondIsSet() {
 		imageA = null;
 		imageB = FILE_1;
 	}
-	
+
 	private void givenSecondParameterIsNullAndFirstIsSet() {
 		imageA = FILE_1;
 		imageB = null;
@@ -60,25 +65,23 @@ public class CompareScreenshotTest {
 	@Test
 	public void ifBothImagesAreEqual_returns0() {
 		givenBothFilesAreEqual();
-		
+
 		whenComparingScreenshots();
-		
+
 		expectReturns0();
 	}
-	
-	
+
 	@Test
 	public void ifImagesAreHalfTheSame_returns50() {
 		givenBothFilesAreHalfEqual();
-		
+
 		whenComparingScreenshots();
-		
+
 		expectReturns50();
 	}
 
 	private void expectReturns50() {
-		  Assert.assertTrue("Error, Difference is to high", 52 >= differenceInPercent);
-		  Assert.assertTrue("Error, Difference is to low",  48  <= differenceInPercent);
+		Assert.assertEquals("Value was:" + differenceInPercent, 50.00, differenceInPercent, accuracy);
 	}
 
 	private void givenBothFilesAreHalfEqual() {
@@ -95,92 +98,168 @@ public class CompareScreenshotTest {
 		imageA = FILE_TOTAL_BLACK;
 		imageB = FILE_TOTAL_WHITE;
 	}
+
 	private void whenComparingScreenshots() {
 		CompareScreenshot test = new CompareScreenshot();
-		differenceInPercent = test.compare(imageA,imageB);
+		differenceInPercent = test.compare(imageA, imageB);
 	}
 
 	private void expectReturns100() {
-		Assert.assertEquals(100, differenceInPercent);
+		Assert.assertEquals("Value was: " + differenceInPercent, 100.00, differenceInPercent, accuracy);
 	}
-	
+
 	@Test
 	public void ifImagesAreTotalDifferent_returns100() {
 		givenBothFilesAreTotalDifferent();
-		
+
 		whenComparingScreenshots();
-		
+
 		expectReturns100();
 	}
-	
+
 	@Test
 	public void ifImageColorAndBigger() {
 		givenImageColorAndBigger();
-		
+
 		whenComparingScreenshots();
-		
-		expectReturnsLessThan50();
+
+		expectReturnsMoreThan50();
 	}
-	
+
 	private void expectReturns0() {
-		Assert.assertEquals(0, differenceInPercent);
+		Assert.assertEquals("Value was" + differenceInPercent, 0.00, differenceInPercent, accuracy);
 	}
-	
-	private void expectReturnsLessThan50() {
-		Assert.assertTrue(differenceInPercent<=50);
+
+	private void expectReturnsMoreThan50() {
+		Assert.assertTrue("Difference was: " + differenceInPercent, differenceInPercent >= 50);
 	}
-	
+
 	private void givenImageColorAndBigger() {
 		imageA = FILE_1;
 		imageB = FILE_2;
 	}
-	
+
 	@Test
 	public void ifImagesAreTotalDifferentBig_returns100() {
 		givenBothFilesAreTotalDifferentBig();
-		
+
 		whenComparingScreenshots();
-		
+
 		expectReturns100();
 	}
 
 	private void givenBothFilesAreTotalDifferentBig() {
 		imageA = FILE_TOTAL_WHITE_300;
 		imageB = FILE_TOTAL_BLACK_300;
-		
+
 	}
-	
+
 	@Test
 	public void ifImagesAreHalfTheSameBig_returns50() {
 		givenBothFilesAreHalfEqualBig();
-		
+
 		whenComparingScreenshots();
-		
+
 		expectReturns50();
 	}
 
 	private void givenBothFilesAreHalfEqualBig() {
 		imageA = FILE_50_300;
-		imageB = FILE_TOTAL_BLACK_300;
+		imageB = FILE_TOTAL_WHITE_300;
 	}
-	
+
 	@Test
-	public void ifImagesAreNotSameSize_expectException() {
-		givenParameterNotSameSize();
-		
-		try {
-			whenComparingScreenshots();
-			Assert.fail();
-		} catch(IllegalArgumentException e) {
-			Assert.assertEquals("Image scale is not the same", e.getMessage());
-		}
+	public void ifImg1HeightBigger() {
+		givenifImg1HeightBigger();
+
+		whenComparingScreenshots();
+
+		expectReturnsMoreThan50();
 	}
 
-	private void givenParameterNotSameSize() {
-		imageA=FILE_TOTAL_BLACK_300;
-		imageB=FILE_TOTAL_BLACK;
-		
+	private void givenifImg1HeightBigger() {
+		imageA = FILE_50_400_300;
+		imageB = FILE_TOTAL_WHITE_300;
 	}
 
-	
+	@Test
+	public void ifImg1WidthBigger() {
+		givenifImg1WidthBigger();
+
+		whenComparingScreenshots();
+
+		expectReturnsMoreThan50();
+	}
+
+	private void givenifImg1WidthBigger() {
+		imageA = FILE_50_300_400;
+		imageB = FILE_TOTAL_WHITE_300;
+
+	}
+
+	@Test
+	public void ifImg2HeightBigger() {
+		givenifImg2HeightBigger();
+
+		whenComparingScreenshots();
+
+		expectReturnsMoreThan50();
+	}
+
+	private void givenifImg2HeightBigger() {
+		imageB = FILE_50_400_300;
+		imageA = FILE_TOTAL_WHITE_300;
+
+	}
+
+	@Test
+	public void ifImg2WidthBigger() {
+		givenifImg2WidthBigger();
+
+		whenComparingScreenshots();
+
+		expectReturnsMoreThan50();
+	}
+
+	private void givenifImg2WidthBigger() {
+
+		imageB = FILE_50_300_400;
+		imageA = FILE_TOTAL_WHITE_300;
+
+	}
+
+	@Test
+	public void ifImagesAreOneQuaterTheSameBig_returns75() {
+		givenBothFilesAreOneQuaterEqualBig();
+
+		whenComparingScreenshots();
+
+		expectReturns75();
+	}
+
+	private void expectReturns75() {
+		Assert.assertEquals("Value was: " + differenceInPercent, 75.00, differenceInPercent, accuracy);
+
+	}
+
+	private void givenBothFilesAreOneQuaterEqualBig() {
+		imageA = FILE_75_300;
+		imageB = FILE_TOTAL_WHITE_300;
+
+	}
+
+	@Test
+	public void ifImagesAreOneQuaterTheSameMassiv_returns75() {
+		givenBothFilesAreOneQuaterEqualMassiv();
+
+		whenComparingScreenshots();
+
+		expectReturns75();
+	}
+
+	private void givenBothFilesAreOneQuaterEqualMassiv() {
+		imageA = FILE_75_2000;
+		imageB = FILE_TOTAL_WHITE_2000;
+	}
+
 }
