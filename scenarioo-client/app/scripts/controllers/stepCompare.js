57c47829feeb71003998c8f2aae22de626bfd693
jordilaforge <8899ph@web.de>
2015-01-27 02:56:49 +0100

    next

--- scenarioo-client/app/scripts/controllers/stepCompare.js
+++ scenarioo-client/app/scripts/controllers/stepCompare.js
@@ -17,7 +17,7 @@
 
 'use strict';
 
-angular.module('scenarioo.controllers').controller('StepCompareCtrl', function ($scope, $location, $routeParams, BranchesAndBuilds, BranchesAndBuildsForComparison, SelectedBranchAndBuild, StepResource, GlobalHotkeysService, HostnameAndPort) {
+angular.module('scenarioo.controllers').controller('StepCompareCtrl', function ($scope, $location, $routeParams, BranchesAndBuilds, BranchesAndBuildsForComparison, SelectedBranchAndBuild, StepResource, GlobalHotkeysService, HostnameAndPort, resemble) {
 
     var useCaseName = $routeParams.useCaseName;
     var scenarioName = $routeParams.scenarioName;
